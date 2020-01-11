c7beb7001fb244e37d6498be55e13ab24253817c
Florian PERIGNE <florian.perigne@gmail.com>
2020-01-11 17:00:07 +0100

    correction de bugs

--- main.cpp
+++ main.cpp
@@ -3,22 +3,6 @@
 #include "solver.hpp"
 
 int main(int argc, char* argv[])
-{
-	
-	solver_edp test(1, 10);
-	
-	std::vector<std::vector<float>> test_mat;
-	
-	test_mat = test.get_matrix();
-	
-	for (int i = 0; i < test_mat.size(); ++i)
-	{
-		for (int j = 0; j < test_mat.size(); ++j)
-		{
-			std::cout << test_mat[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-	
+{	
 	return 0;
 }
--- solver.hpp
+++ solver.hpp
@@ -4,7 +4,7 @@ class solver_edp
 {
 public:
 	
-	solver_edp(const double& maturity, const int& step, const vol_model& vol);
+	solver_edp(const double& maturity, const int& step, std::vector<std::vector<double>> vol);
 	std::vector<std::vector<double>> get_matrix();
 	std::vector<std::vector<double>> get_kappa();
 	
@@ -14,11 +14,3 @@ private:
 	std::vector<std::vector<double>> s_vol;
 	std::vector<double> u0;
 };
-
-class vol_model
-{
-	vol_model(const std::vector<std::vector<double>>& vol);
-	
-private:
-	std::vector<std::vector<double>> v_vol;
-}
