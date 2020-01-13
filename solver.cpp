1bed35ee78b3478cfe071e523ca044d3ca00ef75
Florian PERIGNE <florian.perigne@gmail.com>
2020-01-13 13:30:47 +0100

    Gestion en vecteur des matrices de la PDE, étant données qu'elles sont trigonales, on peut seulement stocker 3 vecteurs. Ajout fonction trig_matmul qui multiplie efficacement une matrice trig avec un vecteur.

--- solver.cpp
+++ solver.cpp
@@ -13,16 +13,16 @@ std::vector<double> solver_edp::solve_pde()
 	return a;
 }
 
-std::vector<double> solver_edp::product_inverse(std::vector<std::vector<double>> mat, std::vector<double> d)
+std::vector<double> solver_edp::product_inverse(std::vector<std::vector<double>> trig_mat, std::vector<double> d)
 {
 	double w;
-	std::vector<double> a(mat[0].size());
-	std::vector<double> b(mat[0].size());
-	std::vector<double> c(mat[0].size());
+	std::vector<double> a(trig_mat[0]);
+	std::vector<double> b(trig_mat[1]);
+	std::vector<double> c(trig_mat[2]);
 	
-	std::vector<double> x(mat[0].size());
+	std::vector<double> x(trig_mat[0].size());
 
-	for(int i=0; i<b.size(); ++i)
+/* 	for(int i=0; i<b.size(); ++i)
 	{
 		b[i] = mat[i][i];
 		
@@ -41,21 +41,40 @@ std::vector<double> solver_edp::product_inverse(std::vector<std::vector<double>>
 			a[i] = mat[i][i-1];
 			c[i] = mat[i][i+1];
 		}
-	}
+	} */
 	
-	for(int i=1; i< mat[0].size(); ++i)
+	for(int i=1; i< trig_mat[0].size(); ++i)
 	{
 		w = a[i]/b[i-1];
 		b[i] = b[i] - w*c[i-1];
 		d[i] = d[i] - w*d[i-1];
 	}
 	
-	x[mat[0].size()-1] = d[mat[0].size()-1]/b[mat[0].size()-1];
+	x[trig_mat[0].size()-1] = d[trig_mat[0].size()-1]/b[trig_mat[0].size()-1];
 
-	for(int i=mat[0].size()-2; i >= 0; --i)
+	for(int i=trig_mat[0].size()-2; i >= 0; --i)
 	{
 		x[i] = (d[i]-c[i]*x[i+1])/b[i];
 	}
 	
 	return x;
+}
+
+std::vector<double> solver_edp::trig_matmul(const std::vector<std::vector<double>>& trig_mat, const std::vector<double>& x)
+{
+	std::vector<double> inf(trig_mat[0]);
+	std::vector<double> diag(trig_mat[1]);
+	std::vector<double> sup(trig_mat[2]);
+	
+	std::vector<double> res(trig_mat[0]);
+	
+	for(int i=1;i<inf.size()-2;i++)
+	{
+		res[i] = diag[i]*x[i] + inf[i]*x[i-1] + sup[i]*x[i+1];
+	}
+	
+	res[0] = diag[0]*x[0] + sup[0]*x[1];
+	res[res.size()-1] = diag[res.size()-1]*x[res.size()-1] + inf[res.size()-1]*x[res.size()-1];
+	
+	return res;
 }
