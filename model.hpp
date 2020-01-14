f95bf2aa153d1cfbc2ea3093690ae17c4018da04
Stackyx <thibnast@hotmail.fr>
2020-01-14 18:21:24 +0100

    commencement de sigma dependant de x

--- model.hpp
+++ model.hpp
@@ -10,7 +10,10 @@ public:
 	model(const double& S0, const std::vector<double>& sigma, const double& r, const double& T, const int& n_t, const int& n_x, const double& theta, payoff& f, std::vector<std::vector<double>> conditions = {{0, 0}, {0,0}}, std::string method = "Dirichelet");
 	model(const double& S0, const double& sigma, const std::vector<double>& r, const double& T, const int& n_t, const int& n_x, const double& theta, payoff& f, std::vector<std::vector<double>> conditions = {{0, 0}, {0,0}}, std::string method = "Dirichelet");
 	model(const double& S0, const std::vector<double>& sigma, const std::vector<double>& r, const double& T, const int& n_t, const int& n_x, const double& theta, payoff& f, std::vector<std::vector<double>> conditions = {{0, 0}, {0,0}}, std::string method = "Dirichelet");
-
+	
+	model(const double& S0, const std::vector<std::vector<double>>& sigma, const double& S_min_mat, const double& S_max_mat, const double& r, const double& T, const int& n_t, const int& n_x, const double& theta, payoff& f, std::vector<std::vector<double>> conditions = {{0, 0}, {0,0}}, std::string method = "Dirichelet");
+	model(const double& S0, const std::vector<std::vector<double>>& sigma, const double& S_min_mat, const double& S_max_mat, const std::vector<double>& r, const double& T, const int& n_t, const int& n_x, const double& theta, payoff& f, std::vector<std::vector<double>> conditions = {{0, 0}, {0,0}}, std::string method = "Dirichelet");
+	
 	std::vector<std::vector<double>> getDirichelet();
 	std::vector<std::vector<double>> getNeumann();
 	
@@ -20,13 +23,15 @@ public:
 	double get_r(const int&i);
 	double get_dx();
 	
+
+	
 private:
 	
 	friend class solver_edp;
 	
 	std::vector<std::vector<double>> m_cdt;
 	double m_initS;
-	std::vector<double> m_sigma;
+	std::vector<std::vector<double>> m_sigma;
 	std::vector<double> m_r;
 
 	double m_dt;
