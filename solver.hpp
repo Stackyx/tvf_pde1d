ecd6a0e2bf459fb2fdc991b5ab3bb02be11b9cb6
Florian PERIGNE <florian.perigne@gmail.com>
2020-01-14 17:56:56 +0100

    Modif des fonctions qui génèrent les matrices de PDE. Elles sont désormais en void et prennent en argument une matrice en ref, ce qui permet d'éviter de réallouer de la mémoire à chaque tour de boucle de solver

--- solver.hpp
+++ solver.hpp
@@ -11,24 +11,10 @@ public:
 	std::vector<double> solve_pde();
 	
 	std::vector<double> product_inverse(std::vector<std::vector<double>> trig_mat, std::vector<double> d);
-	std::vector<double> trig_matmul(const std::vector<std::vector<double>>& trig_mat, const std::vector<double>& x);
+	std::vector<double> trig_matmul(std::vector<std::vector<double>> trig_mat, std::vector<double> x);
 private:
 	model s_pde_model;
 	
 };
 
-
-class greeks
-{
-public:
-	greeks(solver_edp rst_solver);
-	
-	std::vector<double> getDelta();
-	std::vector<double> getGamma();
-	std::vector<double> getTheta();
-	std::vector<double> getVega();
-	
-private:
-	solver_edp m_solver;
-}
 #endif
