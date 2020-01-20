c7591b17495a16bdef8f06f0b1c828f5a3f9382b
Florian PERIGNE <florian.perigne@gmail.com>
2020-01-20 09:59:10 +0100

    début deg de neumann

--- boundaries.cpp
+++ boundaries.cpp
@@ -7,7 +7,7 @@
 bound::bound(payoff f, mesh grille)
 	: b_f(f), b_mesh(grille)
 {
-	 b_method = "Dirichelet";
+	 b_method = "Dirichlet";
 	 b_conditions = {{0,0}, {0,0}};
 }
 
@@ -26,14 +26,14 @@ bound::bound(payoff f, mesh grille, std::string method)
 	b_conditions = {{0,0}, {0,0}};
 	if ((!(CaseSensitiveIsEqual(b_method,"Dirichlet"))) && (!(CaseSensitiveIsEqual(b_method,"Neumann"))))
 	{
-		std::cout<< "please enter Dirichelet or Neumann only" << std::endl;
+		std::cout<< "please enter Dirichlet or Neumann only" << std::endl;
 	}	
 }
 
 bound::bound(payoff f, mesh grille,std::vector<std::vector<double>> conditions)
 	: b_f(f), b_mesh(grille), b_conditions(conditions)
 {
-	b_method = "Dirichelet";
+	b_method = "Dirichlet";
 }
 
 
@@ -81,8 +81,8 @@ void bound::get_boundaries_nocdt(double ri, double ri1, double sigma0, double si
 		double temp_b_up = std::exp(b_mesh.get_Smin())*(payoff(b_f.getname(), b_strikes).getpayoff()(std::exp(b_mesh.get_Smax()) + h) - payoff(b_f.getname(), b_strikes).getpayoff()(std::exp(b_mesh.get_Smax())))/h;
 		double temp_b_down = std::exp(b_mesh.get_Smin())*(payoff(b_f.getname(), b_strikes).getpayoff()(std::exp(b_mesh.get_Smin()) + h) - payoff(b_f.getname(), b_strikes).getpayoff()(std::exp(b_mesh.get_Smin())))/h;
 		
-		sol[0] = (sol[0] - dt*(1./2.*sigma0*sigma0-ri)*temp_b_down + dt/2.*sigma0*sigma0*(2*sol[1]-2*temp_b_down*b_mesh.get_nx())/(b_mesh.get_nx()*b_mesh.get_nx()))/(dt*sigma0*sigma0/(b_mesh.get_nx()*b_mesh.get_nx())+ri*dt+1);
-		sol[b_mesh.get_nx()-1] = (sol[b_mesh.get_nx()-1] - dt*(1./2.*sigma1*sigma1-ri)*temp_b_up + dt/2.*sigma1*sigma1*(2*sol[b_mesh.get_nx()-2]+2*temp_b_up*b_mesh.get_nx())/(b_mesh.get_nx()*b_mesh.get_nx()))/(dt*sigma1*sigma1/(b_mesh.get_nx()*b_mesh.get_nx())+ri*dt+1);
+		sol[0] = (sol[0] - dt*(1./2.*sigma0*sigma0-ri)*temp_b_down + dt/2.*sigma0*sigma0*(2*sol[1]-2*temp_b_down*b_mesh.get_dx())/(b_mesh.get_dx()*b_mesh.get_dx()))/(dt*sigma0*sigma0/(b_mesh.get_dx()*b_mesh.get_dx())+ri*dt+1);
+		sol[b_mesh.get_nx()-1] = (sol[b_mesh.get_nx()-1] - dt*(1./2.*sigma1*sigma1-ri)*temp_b_up + dt/2.*sigma1*sigma1*(2*sol[b_mesh.get_dx()-2]+2*temp_b_up*b_mesh.get_dx())/(b_mesh.get_dx()*b_mesh.get_dx()))/(dt*sigma1*sigma1/(b_mesh.get_dx()*b_mesh.get_dx())+ri*dt+1);
 
 	}
 	
@@ -104,8 +104,8 @@ void bound::get_boundaries_cdt(double ri, double ri1, double sigma0, double sigm
 		double temp_b_down = b_conditions[j][0];
 		double temp_b_up = b_conditions[j][1];
 
-		sol[0] = (sol[0] - dt*(1./2.*sigma0*sigma0-ri)*temp_b_down + dt/2.*sigma0*sigma0*(2*sol[1]-2*temp_b_down*b_mesh.get_nx())/(b_mesh.get_nx()*b_mesh.get_nx()))/(dt*sigma0*sigma0/(b_mesh.get_nx()*b_mesh.get_nx())+ri*dt+1);
-		sol[b_mesh.get_nx()-1] = (sol[b_mesh.get_nx()-1] - dt*(1./2.*sigma1*sigma1-ri)*temp_b_up + dt/2.*sigma1*sigma1*(2*sol[b_mesh.get_nx()-2]+2*temp_b_up*b_mesh.get_nx())/(b_mesh.get_nx()*b_mesh.get_nx()))/(dt*sigma1*sigma1/(b_mesh.get_nx()*b_mesh.get_nx())+ri*dt+1);
+		sol[0] = (sol[0] - dt*(1./2.*sigma0*sigma0-ri)*temp_b_down + dt/2.*sigma0*sigma0*(2*sol[1]-2*temp_b_down*b_mesh.get_dx())/(b_mesh.get_dx()*b_mesh.get_dx()))/(dt*sigma0*sigma0/(b_mesh.get_dx()*b_mesh.get_dx())+ri*dt+1);
+		sol[b_mesh.get_nx()-1] = (sol[b_mesh.get_nx()-1] - dt*(1./2.*sigma1*sigma1-ri)*temp_b_up + dt/2.*sigma1*sigma1*(2*sol[b_mesh.get_nx()-2]+2*temp_b_up*b_mesh.get_dx())/(b_mesh.get_dx()*b_mesh.get_dx()))/(dt*sigma1*sigma1/(b_mesh.get_dx()*b_mesh.get_dx())+ri*dt+1);
 
 	}
 	
